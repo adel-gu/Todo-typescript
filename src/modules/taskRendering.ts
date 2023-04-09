f6e3d6d175a3255ad14045267ca232881562f2dd
adel <adeelguitoun@gmail.com>
2023-04-09 02:17:50 +0100

    Update renderTasks method

--- src/modules/taskRendering.ts
+++ src/modules/taskRendering.ts
@@ -6,26 +6,25 @@ const createTaskElement = (taskId: number, taskTitle: string): HTMLLIElement => 
   taskItem.setAttribute("data-id", `${taskId}`)
 
   const taskItemContent = document.createElement("div")
-  taskItemContent.classList.add("task-item-content flex justify-between items-center h-10 px-2 bg-slate-800 border-slate-500 border-b")
+  // taskItemContent.classList.add("task-item-content flex justify-between items-center h-10 px-2 bg-slate-800 border-slate-500 border-b")
 
   const title = document.createElement("p")
   title.textContent = taskTitle
 
   const deleteBtn = document.createElement("button")
   deleteBtn.textContent = "Delete"
-  deleteBtn.classList.add("text-red-500 active:text-slate-700")
+  // deleteBtn.classList.add("text-red-500 active:text-slate-700")
 
   // Append childes
   taskItem.appendChild(taskItemContent)
   taskItemContent.appendChild(title)
   taskItemContent.appendChild(deleteBtn)
-
   return taskItem
 }
 
 // Rrenders Tasks
 const renderTasks = (tasks: Task[], tasksContainer: HTMLUListElement) => {
-  tasks.forEach(task => tasksContainer.innerHTML += createTaskElement(task.id, task.title))
+  tasks.forEach(task => tasksContainer.appendChild(createTaskElement(task.id, task.title)))
 }
 
 export { renderTasks }
