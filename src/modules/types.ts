7a51fa97866f15e189b63f74cd8f389a93036e95
adel <adeelguitoun@gmail.com>
2023-04-11 00:52:35 +0100

    Add completed attribute, and classes styles

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -1,6 +1,7 @@
 type Task = {
   id: number,
-  title: string
+  title: string,
+  completed: boolean
 }
 
 const elementDivClasses: string[] = ["task-item-content", "flex", "justify-between", "items-center", "h-10", "px-2", "bg-slate-800", "border-slate-500", "border-b"]
@@ -10,9 +11,10 @@ const elementDeletBtnClasses: string[] = ["deleteBtn", "text-red-500", "active:t
 const elementCheckBoxClasses: string[] = ["accent-pink-500", "rounded", "w-4", "h-4", "focus:ring-pink-500", "focus:focus:ring-2", "cursor-pointer"]
 
 const elementTitleClasses: string[] = ["w-full"]
+const elementTitleCompletedClasses: string[] = ["line-through", "text-slate-500"]
 
 const elementCheckTitleDivClasses: string[] = ["flex", "gap-3", "items-center", "grow"]
 
 
-export { elementDivClasses, elementDeletBtnClasses, elementCheckBoxClasses, elementCheckTitleDivClasses, elementTitleClasses }
+export { elementDivClasses, elementDeletBtnClasses, elementCheckBoxClasses, elementCheckTitleDivClasses, elementTitleClasses, elementTitleCompletedClasses }
 export default Task
