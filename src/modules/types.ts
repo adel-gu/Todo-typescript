3a67f7307fa7631e080475a9715e3ccf6b9e5312
adel <adeelguitoun@gmail.com>
2023-04-10 02:18:07 +0100

    Add deleteBtn class

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -5,7 +5,7 @@ type Task = {
 
 const elementDivClasses: string[] = ["task-item-content", "flex", "justify-between", "items-center", "h-10", "px-2", "bg-slate-800", "border-slate-500", "border-b"]
 
-const elementDeletBtnClasses: string[] = ["text-red-500", "active:text-slate-700"]
+const elementDeletBtnClasses: string[] = ["deleteBtn", "text-red-500", "active:text-slate-700"]
 
 
 export { Task, elementDivClasses, elementDeletBtnClasses }
