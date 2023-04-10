e0bb01029a1a7d90cb36985a0f5c1fdae2578784
adel <adeelguitoun@gmail.com>
2023-04-10 02:18:31 +0100

    Add data-id attribute to the delete button

--- src/modules/taskRendering.ts
+++ src/modules/taskRendering.ts
@@ -1,13 +1,14 @@
 import { Task, elementDivClasses, elementDeletBtnClasses } from "./types"
 
+// Add classes to an html element
 const addElementClasses = (classesArr: string[], htmlElement: HTMLElement) => {
   classesArr.forEach(classStr => htmlElement.classList.add(classStr))
 }
 
+// Create the html element for the task
 const createTaskElement = (taskId: number, taskTitle: string): HTMLLIElement => {
   const taskItem = document.createElement("li")
   taskItem.classList.add("task-item")
-  taskItem.setAttribute("data-id", `${taskId}`)
 
   const taskItemContent = document.createElement("div")
   addElementClasses(elementDivClasses, taskItemContent)
@@ -18,6 +19,7 @@ const createTaskElement = (taskId: number, taskTitle: string): HTMLLIElement => 
   const deleteBtn = document.createElement("button")
   deleteBtn.textContent = "Delete"
   addElementClasses(elementDeletBtnClasses, deleteBtn)
+  deleteBtn.setAttribute("data-id", `${taskId}`)
 
   // Append childes
   taskItem.appendChild(taskItemContent)
