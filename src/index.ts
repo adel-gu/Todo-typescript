61c30466918e50ab714ce7bb5325b43406f8b2c7
adel <adeelguitoun@gmail.com>
2023-04-10 22:53:09 +0100

    Render tasks on page loading

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-import { setLocalStorageTasks } from "./modules/localstorage"
+import { tasksFromLS } from "./modules/localstorage"
 import { addAction, deleteAction } from "./modules/taskActions"
 import { renderTasks } from "./modules/taskRendering"
 import { Task } from "./modules/types"
 
 const form = document.querySelector("form") as HTMLFormElement
 const taskContainer = document.querySelector(".task-list") as HTMLUListElement
-const tasks: Task[] = setLocalStorageTasks()
 
 // Render tasks
-renderTasks(tasks, taskContainer)
+document.addEventListener("DOMContentLoaded", () => {
+  const tasks: Task[] = tasksFromLS()
+  renderTasks(tasks, taskContainer)
+})
 
 // Add task action
-addAction(form, taskContainer, tasks)
+addAction(form, taskContainer)
 
 // remove task action
 document.addEventListener("click", () => {
   if (document.activeElement?.classList.contains("deleteBtn")) {
     const deleteBtn = document.activeElement as HTMLButtonElement
-    deleteAction(deleteBtn, taskContainer, tasks)
+    deleteAction(deleteBtn, taskContainer)
   }
 })
 
