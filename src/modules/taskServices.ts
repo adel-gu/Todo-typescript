b34a8287393492c53fd53767628dccfb562edd32
adel <adeelguitoun@gmail.com>
2023-04-10 22:53:45 +0100

    Add the update tasks id method

--- src/modules/taskServices.ts
+++ src/modules/taskServices.ts
@@ -13,7 +13,13 @@ const addTaskItemToTasks = (tasks: Task[], title: string, id: number): Task[] =>
 // Delete Task Item
 const deleteTaskItemFromTasks = (tasks: Task[], taskId: number): Task[] => {
   tasks = tasks.filter(task => task.id !== taskId)
+  updateTaskId(tasks)
   return tasks
+}
+
+// Update task id
+const updateTaskId = (tasks: Task[]) => {
+  tasks.map(task => task.id = tasks.indexOf(task))
 }
 
 export { addTaskItemToTasks, deleteTaskItemFromTasks }
