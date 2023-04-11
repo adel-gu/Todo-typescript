d9bb1179857ccd32656de536cec1786fba2919db
adel <adeelguitoun@gmail.com>
2023-04-11 01:16:39 +0100

    Invoke complete action method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { tasksFromLS } from "./modules/localstorage"
-import { addAction, deleteAction } from "./modules/taskActions"
+import { addAction, completeAction, deleteAction } from "./modules/taskActions"
 import { renderTasks } from "./modules/taskRendering"
 import Task from "./modules/types"
 
@@ -17,9 +17,16 @@ addAction(form, taskContainer)
 
 // remove task action
 document.addEventListener("click", () => {
-  if (document.activeElement?.classList.contains("deleteBtn")) {
+  if (document.activeElement?.classList.contains("delete-btn")) {
     const deleteBtn = document.activeElement as HTMLButtonElement
     deleteAction(deleteBtn, taskContainer)
   }
 })
 
+// Complete Task
+document.addEventListener("input", () => {
+  if (document.activeElement?.classList.contains("check-completed")) {
+    const checkbox = document.activeElement as HTMLInputElement
+    completeAction(checkbox, taskContainer)
+  }
+})
