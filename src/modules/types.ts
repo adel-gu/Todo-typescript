e62414f76f48b9305a378fdaa1d4397976a293bd
adel <adeelguitoun@gmail.com>
2023-04-11 01:15:38 +0100

    Rename classes

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -6,9 +6,9 @@ type Task = {
 
 const elementDivClasses: string[] = ["task-item-content", "flex", "justify-between", "items-center", "h-10", "px-2", "bg-slate-800", "border-slate-500", "border-b"]
 
-const elementDeletBtnClasses: string[] = ["deleteBtn", "text-red-500", "active:text-slate-700"]
+const elementDeletBtnClasses: string[] = ["delete-btn", "text-red-500", "active:text-slate-700"]
 
-const elementCheckBoxClasses: string[] = ["accent-pink-500", "rounded", "w-4", "h-4", "focus:ring-pink-500", "focus:focus:ring-2", "cursor-pointer"]
+const elementCheckBoxClasses: string[] = ["check-completed", "accent-pink-500", "rounded", "w-4", "h-4", "focus:ring-pink-500", "focus:focus:ring-2", "cursor-pointer"]
 
 const elementTitleClasses: string[] = ["w-full"]
 const elementTitleCompletedClasses: string[] = ["line-through", "text-slate-500"]
